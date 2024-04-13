ac035a95d6025b3684e97c882e042cbada6880f7
dolvin17 <huertas876@gmail.com>
2024-04-13 11:20:08 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "@/styles/app.module.css";
+import {
+  DocsCard,
+  HelloComponentsCard,
+  HelloNearCard,
+} from "@/components/cards";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+     
+    </main>
+  );
+}
